e95d4a20105594710036086ca651f223728bf3e2
Robotics Lab <isaachyelngtil@gmail.com>
2021-04-20 13:33:56 +0100

    initial

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,23 +12,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener{
+        rollButton.setOnClickListener {
             Toast.makeText(this, "Rolled!", Toast.LENGTH_SHORT).show()
         }
 
+    //  Displays the resultText of the rolled dice
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    //  creates a text for the result of rolled dice
+    private fun rollDice() {
         val myDice = Dice(6)
-
-// Displays the result of rolled dice
-        rollButton.setOnClickListener{
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = myDice.roll().toString()
-
-        }
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = myDice.roll().toString()
     }
 
 }
+
 class Dice(private val numSides: Int) {
-    fun roll(): Int{
+    fun roll(): Int {
         return (1..numSides).random()
     }
 }
